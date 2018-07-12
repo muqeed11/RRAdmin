6a3c1d3e05908448402db11e2a816b2a00fd4226
Muqeed <muqeed11@gmail.com>
2018-07-12 22:38:40 +0530

    RRAdmin

--- app.js
+++ app.js
@@ -8,10 +8,15 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 var appRoutes = require('./routes/app');
+var loginRoutes = require('./routes/userauth');
+var userprofile = require('./routes/userprofile');
+// var users = require('./routes/users');
+
 
 var app = express();
 
-mongoose.connect('mongodb://localhost:27017/node-angular')
+mongoose.connect('mongodb://localhost:27017/node-angular');
+// mongoose.connect('mongodb://localhost/crud');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -32,7 +37,10 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.user('/sigin',)
+app.use('/userauth',loginRoutes);
+app.use('/userprofile',userprofile);
+// app.use('/getprofile',userprofile);
+// app.use('/users',users);
 app.use('/', appRoutes);
 
 // catch 404 and forward to error handler
