5283108b5fcf1ae1cfd05e693436b5015f271d22
Muqeed <muqeed11@gmail.com>
2018-07-12 22:44:28 +0530

    RRAdmin2

---
+++ models/reports.js
@@ -0,0 +1,21 @@
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var mongooseUniqueValidator = require('mongoose-unique-validator');
+// mongoose.connect('mongodb://localhost:20717/node-angular');
+
+
+var UserSchema = new Schema({
+    userId:{type:String,required:true},
+    password:{type:String,required:true},
+    customerName:{type:String,required:true},
+    active:{type:String ,required:true},
+    createdDate:{type:Date,required:true},
+    validDate:{type:String,required:true},
+    lastUpdated:{type:Date,required:true}
+
+});
+
+UserSchema.plugin(mongooseUniqueValidator);
+
+module.exports = mongoose.model('reports',UserSchema);
