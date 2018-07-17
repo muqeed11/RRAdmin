7444f2880863659f9416c7c734cf3c463b2f7805
Muqeed <muqeed11@gmail.com>
2018-07-17 12:28:25 +0530

    17thJuly

--- routes/listofcustomers.js
+++ routes/listofcustomers.js
@@ -6,7 +6,6 @@ router.post('/customerdetails', function (req, res, next) {
 
     Userprofile.find(function(err,userinfo) {
         console.log("inside listofcustomers")
-        console.log(req.body.userId)
         console.log(userinfo)
         if (err) {
             return res.json({
---
+++ routes/reportsAdmin.js
@@ -0,0 +1,131 @@
+var express = require('express');
+var router = express.Router();
+var Reports = require('../models/reports');
+var mkdirp = require('mkdirp');
+var Userprofile = require('../models/userprofile');
+var fs = require('fs');
+var fse = require('fs-extra');
+var PDFDocument = require('pdfkit');
+
+router.post('/showreportAdmin',function(req,res,next){
+
+    console.log("inside showreport admin")
+    Reports.findOne({_id:req.body.reportId},function (err,result) {
+
+        // console.log(result)
+
+        const reportPath = result.reportKey;
+        const reportType1 = result.reportType;
+        const reportId = result._id
+        fs.readFile(reportPath,'base64',function (err,base64string) {
+            if(!err)
+            {
+                return res.json({
+                    reportFile:base64string,
+                    responseStatus: '0',
+                    reportType : reportType1,
+                    reportId:reportId
+                })
+            }
+            else {
+                return res.json({
+                    responseStatus: '1',
+                    response: 'An Error Occurred while fetching one report'
+                })
+            }
+        });
+    })
+});
+
+router.post('/delreportAdmin',function(req,res,next){
+
+    // console.log("inside delreport admin")
+    // console.log(req.body.reportId )
+    // console.log(req.body.reportReason )
+    // console.log(req.body.reason)
+
+        Reports.updateOne({"_id":req.body.reportId},{$set:{reportStatus:"Deleted",reportReason:req.body.reportReason,
+                lastUpdated:Date.now()}},function (err1, result) {
+            console.log(result)
+
+            if (err1) {
+                return res.status(500).json({
+                    title: 'An Error Occured while saving data in reports table',
+                    error: err1,
+                    responseStatus:'1'
+                });
+            }
+
+            else {
+                return res.json({
+                    reponseStatus:'0',
+                    reportReason : req.body.reportReason
+
+                })
+            }
+        });
+
+});
+
+router.post('/rejreportAdmin',function(req,res,next){
+
+    // console.log("inside delreport admin")
+    // console.log(req.body.reportId )
+    // console.log(req.body.reportReason )
+    // console.log(req.body.reason)
+
+    Reports.updateOne({"_id":req.body.reportId},{$set:{reportStatus:"Rejected",reportReason:req.body.reportReason,
+            lastUpdated:Date.now()}},function (err1, result) {
+        console.log(result)
+
+        if (err1) {
+            return res.status(500).json({
+                title: 'An Error Occured while saving data in reports table',
+                error: err1,
+                responseStatus:'1'
+            });
+        }
+
+        else {
+            return res.json({
+                reponseStatus:'0',
+                reportReason : req.body.reportReason
+
+            })
+        }
+    });
+
+});
+
+router.post('/approvereportAdmin',function(req,res,next){
+
+    // console.log("inside delreport admin")
+    // console.log(req.body.reportId )
+    // console.log(req.body.reportReason )
+    // console.log(req.body.reason)
+
+    Reports.updateOne({"_id":req.body.reportId},{$set:{reportStatus:"Approved",reportReason:req.body.reportReason,
+            lastUpdated:Date.now()}},function (err1, result) {
+        console.log(result)
+
+        if (err1) {
+            return res.status(500).json({
+                title: 'An Error Occured while saving data in reports table',
+                error: err1,
+                responseStatus:'1'
+            });
+        }
+
+        else {
+            return res.json({
+                reponseStatus:'0',
+                reportReason : req.body.reportReason
+
+            })
+        }
+    });
+
+});
+
+
+module.exports = router;
